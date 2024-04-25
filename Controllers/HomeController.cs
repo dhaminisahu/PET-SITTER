34b40b07fbc5c5239cb0a75733f3b5e76a72d2bf
dhaminisahu <126551291+dhaminisahu@users.noreply.github.com>
2024-04-25 21:11:03 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace furni.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+		public ActionResult About()
+		{
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			// Redirect to the 'Create' action of 'UserInformationsController'
+			return RedirectToAction("Create", "UserInformations");
+		}
+		public ActionResult Services()
+		{
+			return View();
+		}
+		
+	}
+}
---
+++ Controllers/UserInformationsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace furni.Controllers
+{
+    public class UserInformationsController : Controller
+    {
+        private UserDataEntities2 db = new UserDataEntities2();
+
+        // GET: UserInformations
+        public ActionResult Index()
+        {
+            return View(db.UserInformations.ToList());
+        }
+
+        // GET: UserInformations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInformation userInformation = db.UserInformations.Find(id);
+            if (userInformation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInformation);
+        }
+
+        // GET: UserInformations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserInformations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Email,Subject,Message")] UserInformation userInformation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserInformations.Add(userInformation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userInformation);
+        }
+
+        // GET: UserInformations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInformation userInformation = db.UserInformations.Find(id);
+            if (userInformation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInformation);
+        }
+
+        // POST: UserInformations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Email,Subject,Message")] UserInformation userInformation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userInformation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userInformation);
+        }
+
+        // GET: UserInformations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInformation userInformation = db.UserInformations.Find(id);
+            if (userInformation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInformation);
+        }
+
+        // POST: UserInformations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserInformation userInformation = db.UserInformations.Find(id);
+            db.UserInformations.Remove(userInformation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
